e4a6a0f80ab1358aed9a6e0811664606208110a7
Shital Savekar <iamshital@users.noreply.github.com>
2017-12-05 16:46:12 -0800

    test

---
+++ PowerShell/Accept-VHD.ps1
@@ -0,0 +1,190 @@
+﻿Param( $BuildNumber,
+$JobUser
+)
+try
+{
+    $VHD=$env:VHD
+    $RegionVMsize=$env:RegionVMsize
+    $TestSuite=$env:TestSuite
+    $Email=$env:Email
+    $customKernel = $env:customKernel
+
+
+    $exitValue = 1
+    Import-Module J:\Jenkins\userContent\vendortesting.psm1 -Force -Global
+
+    $missingParameters = @()
+    $exitValue = 1
+    #Validate Parameters
+    If(!(Test-Path VHD -ErrorAction SilentlyContinue))
+    {
+        $missingParameters += "VHD"
+        #Throw "Missing VHD file."
+    }
+    If($RegionVMsize -eq $null)
+    {
+        $missingParameters += "RegionVMsize"
+        #Throw "Missing parameter 'RegionVMsize'."
+    }
+    If($Email -eq $null)
+    {
+        $missingParameters += "Email"
+        #Throw "Missing parameter 'Email'."
+    }
+    If($TestSuite -eq $null)
+    {
+        $missingParameters += "TestSuite"
+        #Throw "Missing parameter 'TestSuite'."
+    }
+
+    if ($missingParameters.Count -gt 0 )
+    {
+        $j = 1
+        
+        Write-Host "Following parameters are missing - "
+        foreach($parameter in $missingParameters)
+        {
+            Write-Host "`t$j`t$parameter" 
+            $j += 1  
+        }
+        Throw "MISSING_PARAMETERS_EXCEPTION"
+    }
+
+
+    $userContentHomeDir = "J:\Jenkins\userContent"
+
+    $7zExePath = "J:\Jenkins\userContent\azure-linux-automation\tools\7za.exe"
+    #$vhdPath = "J:\temp\redhat72-demo-dynamic.vhd.xz"
+    $vhdPath = $VHD
+    Write-Host "Job builder - $JobUser"
+
+    if ($VHD)
+    {
+        Remove-Item -Path $VHD -Force -ErrorAction SilentlyContinue
+        Write-Host "Using user provided file $VHD ..."
+        Rename-Item -Path VHD -NewName $VHD
+    
+        if (($VHD).ToLower().EndsWith("xz"))
+        {
+            Write-Host "Extracting $VHD. Please wait..."
+            $7zConsoleOuput = Invoke-Expression -Command "$7zExePath -y x $vhdPath;"
+            if ($7zConsoleOuput -imatch "Everything is Ok")
+            {
+                Write-Host "Extraction completed."
+
+                $vhdActualSize = ($7zConsoleOuput -imatch "size").Replace(" ",'').Replace(" ",'').Replace(" ",'').Replace(" ",'').Split(":")[1]
+                $vhdCompressedSize = ($7zConsoleOuput -imatch "Compressed").Replace(" ",'').Replace(" ",'').Replace(" ",'').Replace(" ",'').Split(":")[1]
+                $compressinRatio = ((($vhdCompressedSize/($vhdActualSize-$vhdCompressedSize))*100))
+            
+                Write-Host "Compression Ratio : $compressinRatio%"
+                $newVHDName = ($VHD).TrimEnd("xz").TrimEnd(".")
+                Write-Host "Removing file $VHD"
+                Remove-Item -Path $VHD -Force
+            }
+            else
+            {
+                Throw "Error: Failed to extract $VHD."
+            }
+        }
+        elseif (($VHD).ToLower().EndsWith("vhd") -or ($VHD).ToLower().EndsWith("vhdx"))
+        {
+            $newVHDName = ($VHD)
+        }
+        else
+        {
+            Write-Host "$($VHD) is NOT supported. Supported file formats: vhd, vhdx and xz (compressed VHD/VHDX) file."
+            Throw "UNSUPPORTED_FILE_EXCEPTION"
+        }
+
+        Write-Host "Creating $newVHDName.json file..."
+
+
+        if ($env:MultipleTestCycles)
+        {
+        #Prepare Multiple Test JSON file.
+
+        }
+        else
+        {
+            #region Prepare Single Test JSON file.
+            $testRegion = (($RegionVMsize).Split(" ")[0]).Trim()
+            $testVMSize = (($RegionVMsize).Split(" ")[1]).Trim()
+            $singleTestJSONText = Get-Content "$userContentHomeDir\JSON\SingleTestJSON.json"
+            $singleTestJSONText = $singleTestJSONText.Replace("CustomTestCycleName","$TestSuite")
+            $singleTestJSONText = $singleTestJSONText.Replace("CustomVMSize","$testVMSize")
+            $singleTestJSONText = $singleTestJSONText.Replace("CustomRegion","$testRegion")
+            $singleTestJSONText = $singleTestJSONText.Replace("CustomPublisher","$JobUser")
+            $singleTestJSONText = $singleTestJSONText.Replace("CustomEmail","$Email")
+            $singleTestJSONText = $singleTestJSONText.Replace("custom_Kernel","$customKernel")
+            if ($env:GitRepo)
+            {
+                $singleTestJSONText = $singleTestJSONText.Replace("iamshital","$env:GitRepo")
+            }
+            if ($env:GitBranch)
+            {
+                $singleTestJSONText = $singleTestJSONText.Replace("ostcci","$env:GitBranch")
+            }
+            #Write-Host $singleTestJSONText
+            Set-Content -Value $singleTestJSONText -Path "$newVHDName.json" -Force
+            Write-Host "Created $newVHDName.json file..."
+            #endregion
+        }
+
+        #Validate VHD before upload.
+        ValidateVHD -vhdPath $newVHDName
+        $jsonItem = "$newVHDName.json"
+        $jsonTxt = [string](Get-Content -Path "$jsonItem")
+        $jsonObj = ConvertFrom-Json -InputObject $jsonTxt
+        if ( $jsonObj.md5)
+        {
+            ValidateMD5 -filePath $newVHDName -expectedMD5hash $jsonObj.md5
+        }
+        if ( $jsonObj.publisher -imatch "suse")
+        {
+            $monitorDir = "Q:\suse-ftp-rw"
+        }
+        if ( $jsonObj.publisher -imatch "redhat")
+        {
+            $monitorDir = "Q:\redhat-ftp-rw"
+        }
+        if ( $jsonObj.publisher -imatch "suse")
+        {
+            $monitorDir = "Q:\suse-ftp-rw"
+        }
+        if ( $jsonObj.publisher -imatch "ubuntu")
+        {
+            $monitorDir = "Q:\ubuntu-ftp-rw"
+        }
+        if ( $jsonObj.publisher -imatch "msft")
+        {
+            $monitorDir = "Q:\msftuser-ftp-rw"
+        }
+
+        Write-Host "Moving $newVHDName to $($jsonObj.publisher) queue directory."
+        
+        Move-Item -Path $newVHDName -Destination $monitorDir -Force
+        
+        Write-Host "Moving $newVHDName.json to $($jsonObj.publisher) queue directory."
+        Move-Item -Path "$newVHDName.json" -Destination $monitorDir -Force
+        Write-Host "Your VHD is received successfully."
+        Write-Host "You will receive email notifications, when-"
+        Write-Host "  1. we upload VHD to azure storage account."
+        Write-Host "  2. we start the tests."
+        Write-Host "  3. we finish the tests."
+        $exitValue = 0    
+    }
+    else
+    {
+        Write-Host "Error: You did not provide any VHD file."
+        $exitValue = 1
+    }
+}
+catch
+{
+    Write-Host $_.Exception.GetType().FullName, " : ",$_.Exception.Message
+    exit 1
+}
+finally
+{
+    exit $exitValue
+}
---
+++ PowerShell/vendortesting.psm1
@@ -0,0 +1,230 @@
+﻿Function GetStorageAccountByLocationAndVMSize($location, $vmSize)
+{
+    #Write-Host "$location, $vmSize"
+
+    if ( ( $location -eq "australiaeast" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkaciaustraliaestxiov2"
+    }
+    elseif ( ( $location -eq "australiaeast" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkaciaustraliaeastv2"
+    }
+    elseif ( ( $location -eq "australiasoutheast" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkaciaussouthestxiov2"
+    }
+    elseif ( ( $location -eq "australiasoutheast" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkaciaussouthestv2"
+    }
+    elseif ( ( $location -eq "brazilsouth" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacibrazilsouthxiov2"
+    }
+    elseif ( ( $location -eq "brazilsouth" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacibrazilsouthv2"
+    }
+    elseif ( ( $location -eq "canadacentral" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacicanadacntralxiov2"
+    }
+    elseif ( ( $location -eq "canadacentral" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacicanadacentralv2"
+    }
+    elseif ( ( $location -eq "canadaeast" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacicanadaeastxiov2"
+    }
+    elseif ( ( $location -eq "canadaeast" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacicanadaeastv2"
+    }
+    elseif ( ( $location -eq "centralus" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacicentralusxiov2"
+    }
+    elseif ( ( $location -eq "centralus" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacicentralusv2"
+    }
+    elseif ( ( $location -eq "eastasia" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacieastasiaxiov2"
+    }
+    elseif ( ( $location -eq "eastasia" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacieastasiav2"
+    }
+    elseif ( ( $location -eq "eastus" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacieastusxiov2"
+    }
+    elseif ( ( $location -eq "eastus" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacieastusv2"
+    }
+    elseif ( ( $location -eq "eastus2" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacieastus2xiov2"
+    }
+    elseif ( ( $location -eq "eastus2" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacieastus2v2"
+    }
+    elseif ( ( $location -eq "japaneast" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacijapaneastxiov2"
+    }
+    elseif ( ( $location -eq "japaneast" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacijapaneastv2"
+    }
+    elseif ( ( $location -eq "northcentralus" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacinorthcntrlusxiov2"
+    }
+    elseif ( ( $location -eq "northcentralus" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacinorthcentralusv2"
+    }
+    elseif ( ( $location -eq "northeurope" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacineuropexiov2"
+    }
+    elseif ( ( $location -eq "northeurope" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacineuropev2"
+    }
+    elseif ( ( $location -eq "southcentralus" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacisouthcntrlusxiov2"
+    }
+    elseif ( ( $location -eq "southcentralus" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacisouthcentralusv2"
+    }
+    elseif ( ( $location -eq "southeastasia" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacisouthestasiaxiov2"
+    }
+    elseif ( ( $location -eq "southeastasia" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacisoutheastasiav2"
+    }
+    elseif ( ( $location -eq "southindia" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacisouthindiaxiov2"
+    }
+    elseif ( ( $location -eq "southindia" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkacisouthindiav2"
+    }
+    elseif ( ( $location -eq "ukwest" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkaciukwestxiov2"
+    }
+    elseif ( ( $location -eq "ukwest" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkaciukwestv2"
+    }
+    elseif ( ( $location -eq "westcentralus" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkaciwestcentrlusxiov2"
+    }
+    elseif ( ( $location -eq "westcentralus" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkaciwestcentralusv2"
+    }
+    elseif ( ( $location -eq "westeurope" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkaciwesteuropexiov2"
+    }
+    elseif ( ( $location -eq "westeurope" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkaciwesteuropev2"
+    }
+    elseif ( ( $location -eq "westus" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkaciwestusxiov2"
+    }
+    elseif ( ( $location -eq "westus" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkaciwestusv2"
+    }
+    elseif ( ( $location -eq "westus2" ) -and (($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkaciwestus2xiov2"
+    }
+    elseif ( ( $location -eq "westus2" ) -and (!($vmSize -match 'DS' -or $vmSize -match 'GS' -or ($vmSize.Trim().EndsWith("s")) )) )
+    {
+        $storageAccount = "konkaciwestus2v2"
+    }
+    #Write-Host "Selected Storage Account : $storageAccount"
+    return $storageAccount
+}
+
+Function ValidateVHD($vhdPath)
+{
+    try
+    {
+        $tempVHDName = Split-Path $vhdPath -leaf
+        Write-Host "Inspecting $tempVHDName..."
+        $vhdInfo = Get-VHD -Path $vhdPath -ErrorAction Stop
+        Write-Host "  VhdFormat            :$($vhdInfo.VhdFormat)"
+        Write-Host "  VhdType              :$($vhdInfo.VhdType)"
+        Write-Host "  FileSize             :$($vhdInfo.FileSize)"
+        Write-Host "  Size                 :$($vhdInfo.Size)"
+        Write-Host "  LogicalSectorSize    :$($vhdInfo.LogicalSectorSize)"
+        Write-Host "  PhysicalSectorSize   :$($vhdInfo.PhysicalSectorSize)"
+        Write-Host "  BlockSize            :$($vhdInfo.BlockSize)"
+        Write-Host "Validation successful."
+    }
+    catch
+    {
+        Write-Host "Failed: Get-VHD -Path $vhdPath"
+        Throw "INVALID_VHD_EXCEPTION"
+    }
+}
+
+Function ValidateMD5($filePath, $expectedMD5hash)
+{
+    Write-Host "Verifying MD5 of $filePath"
+    $hash = Get-FileHash -Path $filePath -Algorithm MD5
+    if ($hash.Hash.ToUpper() -eq  $expectedMD5hash.ToUpper())
+    {
+        Write-Host "MD5 checksum verified successfully: $($hash.Hash)"
+    }
+    else
+    {
+        Write-host "Expected: $expectedMD5hash"
+        Write-Host "Calculated: $($hash.Hash)"
+        Throw "MD5 checksum verification failed."
+    }
+}
+
+function Test-FileLock {
+  param (
+    [parameter(Mandatory=$true)][string]$Path
+  )
+
+  $oFile = New-Object System.IO.FileInfo $Path
+
+  if ((Test-Path -Path $Path) -eq $false) {
+    return $false
+  }
+
+  try {
+    $oStream = $oFile.Open([System.IO.FileMode]::Open, [System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::None)
+
+    if ($oStream) {
+      $oStream.Close()
+    }
+    $false
+  } catch {
+    # file is locked by a process.
+    return $true
+  }
+}
